5a75a5cdcf4ccac9b74afb91fb8ac256eec891df
vinita <vinitasingh069@gmail.com>
2022-02-02 08:32:19 +0530

    added fn headers

--- math.py
+++ math.py
@@ -1,10 +1,12 @@
-
+#add implementation
 def add(x,y):
     pass
-
+#substract implementation
 def substract(x,y):
     pass
+#multiply implementation
 def multiply(x,y):
     pass
+#divide implementation
 def divide(x,y):
     pass
